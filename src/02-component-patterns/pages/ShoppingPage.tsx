b3b1775283952ed5ac7fe11455d2d9270434f81c
tonovarela <tonovarela@live.com>
2023-01-03 17:50:59 -0600

    fin de la seccion

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,7 +1,7 @@
-import { ProductCard, } from "../components";
-import '../styles/custom-style.css';
+import { ProductButtons, ProductCard, ProductImage, ProductTitle, } from "../components";
+
 import { products } from "../data/products";
-import { jsxOpeningElement } from "@babel/types";
+
 const product = products[0];
 export const ShoppingPage = () => {
 
@@ -9,18 +9,14 @@ export const ShoppingPage = () => {
     <div className="bg-dark">
       ShoppingPage
       <hr />
-      <ProductCard key={product.id} product={product} className="bg-dark text-white" initialValues={{ count: 4, maxCount: 15 }} >
+      <ProductCard key={product.id} product={product} initialValues={{ count: 4, maxCount: 15 }} >
         {
           ({ reset, isMaxCountReached, count, increaseBy }) => (
             <>
-
-              <ProductCard.Image className="custom-image"></ProductCard.Image>
-              <ProductCard.Title className="text-white text-bold"  ></ProductCard.Title>
-              <ProductCard.Buttons className="custom-buttons" style={{ display: 'flex', justifyContent: "center" }}></ProductCard.Buttons>
-              <button onClick={reset} >Reset</button>
-              <button onClick={() => increaseBy(-2)}>-2</button>
-              {!isMaxCountReached && <button onClick={() => increaseBy(+2)}>+2</button>}
-              <h4>{count}</h4>
+              <ProductImage/>
+              <ProductTitle/>
+              <ProductButtons/>
+              
             </>
           )
         }
